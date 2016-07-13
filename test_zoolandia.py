e36f080f4baf85502ab842fb5c88ad40fc593b58
whitney-mitchell <whitney.mitchell84@gmail.com>
2016-07-13 09:41:18 -0500

    More tests working. Tests for locomotion, habitat, species.

--- test_zoolandia.py
+++ test_zoolandia.py
@@ -3,18 +3,37 @@ import zoolandia
 
 class TestAnimal(unittest.TestCase):
 
-	@classmethod
-	def setUpClass(self):
-		pass
+	# @classmethod
+	# def setUpClass(self):
+	# 	pass
 
-	def test_name_empty_string_default(self):
-		a_animal = zoolandia.Animal()
-		self.assertEquals(a_animal.name, '')
+	def test_animal_creation(self):
+		a_animal = zoolandia.Betta('orange', 'Bob')
+		self.assertEquals(a_animal.species.color, 'orange')
+		self.assertEquals(a_animal.name, 'Bob')
+		self.assertIsInstance(a_animal, zoolandia.Animal)
+		self.assertIsInstance(a_animal.species, zoolandia.Species)
+		self.assertIsInstance(a_animal.species, zoolandia.BettaTrifasciata)
 
-	def test_species_none_default(self):
-		a_animal = zoolandia.Animal()
-		self.assertEquals(a_animal.species, None)
+	def test_locomotion_initialization(self):
+		a_animal = zoolandia.Betta('orange', 'Bob')
+		self.assertIsInstance(a_animal.locomotion, set)
+		self.assertEquals(0, len(a_animal.locomotion))
 
+	def test_add_locomotion(self):
+		a_animal = zoolandia.Betta('orange', 'Bob')
+		swim = zoolandia.Swimming()
+		a_animal.add_locomotion(swim)
+		self.assertIn(swim, a_animal.locomotion)
+
+	def test_remove_locomotion(self):
+		a_animal = zoolandia.Betta('orange', 'Bob')
+		swim = zoolandia.Swimming()
+		walk = zoolandia.Walking()
+		a_animal.add_locomotion(walk)
+		a_animal.remove_locomotion(swim)
+		self.assertNotIn(swim, a_animal.locomotion)
+		self.assertIn(walk, a_animal.locomotion)
 
 class TestSpecies(unittest.TestCase):
 
@@ -37,6 +56,55 @@ class TestHabitat(unittest.TestCase):
 		habitat = zoolandia.Habitat()
 		self.assertIsInstance(habitat.members, set)
 
+	def test_add_member(self):
+		aquarium = zoolandia.Aquarium("freshwater")
+		a_animal = zoolandia.Betta('orange', 'Bob')
+		aquarium.add_member(a_animal)
+		self.assertIn(a_animal, aquarium.members)
 
+	def test_remove_member(self):
+		aquarium = zoolandia.Aquarium("freshwater")
+		jj = zoolandia.Betta('blue', "JJ")
+		aquarium.add_member(jj)
+		aquarium.remove_member(jj)
+		self.assertNotIn(jj, aquarium.members)
+
+class TestWalking(unittest.TestCase):
+
+	def test_legs_zero_default(self):
+		walking = zoolandia.Walking()
+		self.assertEquals(walking.legs, 0)
+
+	def test_walk_speed_zero_default(self):
+		walking = zoolandia.Walking()
+		self.assertEquals(walking.walk_speed, 0)
+
+
+class TestSwimming(unittest.TestCase):
+
+	def test_appendages(self):
+		swimming = zoolandia.Swimming()
+		self.assertEquals(swimming.fins, False)
+		self.assertEquals(swimming.flippers, False)
+		self.assertEquals(swimming.web_feet, False)
+
+	def test_swim_speed_zero_default(self):
+		swimming = zoolandia.Swimming()
+		self.assertEquals(swimming.swim_speed, 0)
+
+
+class TestFlying(unittest.TestCase):
+
+	def test_wings_zero_default(self):
+		flying = zoolandia.Flying()
+		self.assertEquals(flying.wings, 0)
+
+	def test_wing_span_zero_default(self):
+		flying = zoolandia.Flying()
+		self.assertEquals(flying.wing_span, 0)
+
+	def test_air_speed_zero_default(self):
+		flying = zoolandia.Flying()
+		self.assertEquals(flying.air_speed, 0)
 if __name__ =='__main__':
 		unittest.main()
--- zoolandia.py
+++ zoolandia.py
@@ -9,20 +9,67 @@
 # A base Habitat class.
 # A class definition for each habitat we need in our zoo to hold our initial 8 animals.
 # Each habitat will hold at least one animal, as an aggregation.
-class Animal:
 
-	def __init__(self):
-		self.species = None
-		self.name = ''
+# For animals.py: just import from species, class Animal:..., class Betta(Animal):...
+
+class Animal:
+	def __init__(self, name, species):
+		self.species = species
+		self.name = name
+		self.locomotion = set()
+
+	def add_locomotion(self, loco):
+		self.locomotion.add(loco)
+
+	def remove_locomotion(self, loco):
+		self.locomotion.discard(loco)
 
 class Species:
-
 	def __init__(self):
 		self.common_name = ''
 		self.geo_region = ''
 
-class Habitat:
+class BettaTrifasciata(Species):
+	def __init__(self, color):
+		self.color = color
+		self.common_name = 'Betta'
 
+class Betta(Animal):
+	def __init__(self, color, name):
+		Animal.__init__(self, name, BettaTrifasciata(color))
+# inits a betta that is an animal that inits a BettaTri... that inits a species
+# Animal. could instead be super(). (in Python 3), referencing the parent object, here Animal.
+
+class Habitat:
 	def __init__(self):
 		self.name = ''
 		self.members = set()
+
+	def add_member(self, member):
+		self.members.add(member)
+
+	def remove_member(self, member):
+		self.members.remove(member)
+
+class Aquarium(Habitat):
+	def __init__(self, water_type):
+		Habitat.__init__(self)
+		self.water_type = water_type
+
+class Walking:
+	def __init__(self):
+		self.walk_speed = 0
+		self.legs = 0
+
+class Flying:
+	def __init__(self):
+		self.air_speed = 0
+		self.wings = 0
+		self.wing_span = 0
+
+class Swimming:
+	def __init__(self):
+		self.swim_speed = 0
+		self.fins = False
+		self.flippers = False
+		self.web_feet = False
